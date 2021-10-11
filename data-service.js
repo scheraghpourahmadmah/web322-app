03d0a3a63b4be21fc236c0731b5530fcfa178785
scheraghpourahmadmah <scheraghpourahmadmah@myseneca.ca>
2021-10-10 21:29:45 -0400

    assignment2 finishing touches

--- data-service.js
+++ data-service.js
@@ -1,25 +1,28 @@
+
+// const employees = require("./data/employees.json");
+// const departments = require("./data/departments.json");
+//import * as fs from 'fs/promises';
+
 var employees = [];
 var departments = [];
-var fs = require ("fs");
+var fs = require("fs");
 
 module.exports.initialize = function () {
-    var promise = new Promise((resolve,reject) => {
-        try{
-            fs.readFile('./data/employees.json',(err, data) =>{
-                if(err) throw err;
+    var promise = new Promise((resolve, reject) => {
+        try {
+            fs.readFile('./data/employees.json', (err, data) => {
+                if (err) throw err;
 
                 employees = JSON.parse(data);
-                employees.log("INITIALIZE - load employees.");
             })
-            fs.readFile('./data/depatments.json',(err, data) =>{
-                if(err) throw err;
-                
-                depatments = JSON.parse(data);
-                employees.log("INITALIZE - load depatments.")
+            fs.readFile('./data/departments.json', (err, data) => {
+                if (err) throw err;
+
+                departments = JSON.parse(data);
             })
 
         }
-        catch (ex){
+        catch (ex) {
             console.log("INITIALIZE - FAILURE.");
             reject("INITIALIZE - FAILURE.");
         }
@@ -29,33 +32,54 @@ module.exports.initialize = function () {
     return promise;
 };
 
+module.exports.getAllEmployees = function () {
 
-module.exports.getManagers= function (){
-    var aManagers =[];
-    var promise = new Promise((resolve,reject) => {
-        for (var i =0; i < employees.length; i++){
-            if(employees[i].isManager == true){
-            aManagers.push(employees[i]);
+    var promise = new Promise((resolve, reject) => {
+        if (employees.length === 0) {
+            var err = "getAllEmployees() no results returned.";
+            reject({ message: err });
         }
-    }
-    if(aManagers.length === 0){
-        var err ="getManagers() no results returned.";
-        reject({message:err});
-    }
-    resolve (aManagers);
+        resolve(employees);
     })
-return promise;
+    return promise;
 
 };
-module.exports.getDepartments= function (){
-   
-    var promise = new Promise((resolve,reject) => {
-        if(departments.length === 0){
-            var err ="getDepartments() no results returned.";
-            reject({message:err});
+
+module.exports.getManagers = function () {
+    var aManagers = [];
+    var promise = new Promise((resolve, reject) => {
+        for (var i = 0; i < employees.length; i++) {
+            if (employees[i].isManager == true) {
+                aManagers.push(employees[i]);
+            }
         }
-        resolve (departments);
+
+        if (aManagers.length === 0) {
+            var err = "getManagers() no results returned.";
+            reject({ message: err });
+        }
+
+        resolve(aManagers);
     })
-return promise;
+    return promise;
 
 };
+
+module.exports.getDepartments = function () {
+
+    var promise = new Promise((resolve, reject) => {
+        if (departments.length === 0) {
+            var err = "getDepartments() no results returned.";
+            reject({ message: err });
+        }
+        resolve(departments);
+    })
+    return promise;
+
+};
+
+// function getManagers () {
+//     return employees.filter(employee => {
+//         return employee.isManager == true;
+//     });
+// }
--- server.js
+++ server.js
@@ -9,82 +9,74 @@
  * 
  *  ********************************************************************************/
 
- var dataserver = require("./data-service.js");
- 
- const express = require("express");
- const app = express();
+var dataserver = require("./data-service.js");
+var path = require("path");
 
- const HTTP_PORT = process.env.PORT || 8080;
+const express = require("express");
+const app = express();
+
+const HTTP_PORT = process.env.PORT || 8080;
 
 function onHttpStart() {
-    console.log("Express http server listening on: " + HTTP_PORT);
-  }
-  app.use(express.static('public'));
+  console.log("Express http server listening on: " + HTTP_PORT);
+}
 
-app.get('/', function(req, res) {
-    res.sendFile(__dirname + '/views/home.html');
-  });
-app.get("/about",function (req, res)  {
-    res.sendFile(__dirname +'/views/about.html');
+app.use(express.static('public'));
+
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname + '/views/home.html'));
 });
 
-app.get("/employess",function(req,res){
-dataserver.getAllEmployees()
-
-.then((data) =>{
-  console.log("getAllEmployees JSON.");
-  res.json(data);
-
-})
-.catch((err) =>{
-  console.log(err);
-  res.json(err);
-})
+app.get("/about", function (req, res) {
+  res.sendFile(path.join(__dirname, "/views/about.html"));
 });
 
-
-app.get("/managers",function(req,res){
-  dataserver.getManagers()
-  
-  .then((data) =>{
-    console.log("getManagers JSON.");
-    res.json(data);
-  
-  })
-  .catch((err) =>{
-    console.log(err);
-    res.json(err);
-  })
-  });
-
-  app.get("/departments",function(req,res){
-    dataserver.getDepartments()
-    
-    .then((data) =>{
-      console.log("getDepartments JSON.");
+app.get("/employees", function (req, res) {
+  dataserver.getAllEmployees()
+    .then((data) => {
+      console.log("getAllEmployees JSON.");
       res.json(data);
-    
     })
-    .catch((err) =>{
+    .catch((err) => {
       console.log(err);
       res.json(err);
     })
-    });
+});
 
-    app.use(function(req,res){
-      res.status(404).sendFile(path.join(__dirname + '/views/error404.html'));
+app.get("/departments", function (req, res) {
+  dataserver.getDepartments()
+    .then((data) => {
+      console.log("getDepartments JSON.");
+      res.json(data);
+
     })
-
-
-
-    console.log ("Ready for initialize");
-    dataserver.initialize()
-    .then(() =>{
-      console.log("initialize.then");
-      // setup http server to listen on HTTP_PORT
-       app.listen(HTTP_PORT, onHttpStart);
-    })
-    .catch(err => {
+    .catch((err) => {
       console.log(err);
+      res.json(err);
     })
+});
 
+app.get("/managers", function (req, res) {
+  dataserver.getManagers()
+    .then((data) => {
+      console.log("getManagers JSON.");
+      res.json(data);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.json(err);
+    })
+});
+
+app.use(function (req, res) {
+  res.status(404).sendFile(path.join(__dirname + '/views/error404.html'));
+})
+
+
+// setup http server to listen on HTTP_PORT]
+dataserver.initialize().then(
+  app.listen(HTTP_PORT, onHttpStart)
+).catch(err => {
+  console.log(err);
+});
